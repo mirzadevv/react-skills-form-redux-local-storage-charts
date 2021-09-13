b71c3e189f9c05211d1492abd0cf8d4d0912dec7
mirzaei.dev <mirzaei.devv@gmail.com>
2021-09-13 23:53:03 +0430

    refactor: by choosing every skill in the multiple select, they will apear seperately like tags

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,18 +1,18 @@
 import React, { useState } from "react";
-import { Select } from "antd";
 import { useDispatch } from "react-redux";
 import { v4 as uuidv4 } from "uuid";
+import { Select } from "antd";
 const { Option } = Select;
 
 const Form = () => {
   const dispatch = useDispatch();
-  var [skills, setSkills] = useState({});
   const [data, setData] = useState({
     fullName: "",
     birthDate: "",
     skills: [],
   });
   const [errors, setErrors] = useState({});
+  const [skills, setSkills] = useState([]);
 
   const handleValidate = () => {
     const errors = {};
@@ -42,76 +42,99 @@ const Form = () => {
         skills: skills,
       },
     });
+    data.fullName = "";
+    data.birthDate = "";
+    setSkills([]);
   };
 
   function handleSelectChange(value) {
-    console.log(`selected ${value}`);
-    const skillObj = {
-      id: uuidv4(),
-      value: value,
-    };
-    console.log("skillObj", skillObj);
-    setSkills(skillObj);
+    const haveFound = skills.find((skill) => skill.value === value);
+    if (skills.length === 0 || !haveFound) {
+      const skillObject = {
+        id: uuidv4(),
+        value: value,
+      };
+      const newSkills = [...skills];
+      newSkills.push(skillObject);
+      setSkills(newSkills);
+    }
   }
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <div className="form-group">
-        <input
-          type="text"
-          className="form-control"
-          id="fullName"
-          name="fullName"
-          placeholder="Full Name:"
-          value={data.fullName}
-          onChange={(e) => handleChange(e)}
-        />
-        <span style={{ color: "red", fontSize: "0.7rem" }}>
-          {errors?.errors?.fullName}
-        </span>
-      </div>
-      <br />
-      <div className="form-group">
-        <input
-          type="date"
-          className="form-control"
-          id="birthDate"
-          name="birthDate"
-          placeholder="BirthDate:"
-          value={data.birthDate}
-          onChange={(e) => handleChange(e)}
-        />
-        <span style={{ color: "red", fontSize: "0.7rem" }}>
-          {errors?.errors?.birthdate}
-        </span>
-      </div>
-      <br />
-      <Select
-        mode="multiple"
-        style={{ width: "100%" }}
-        placeholder="select your skill"
-        onChange={handleSelectChange}
-        optionLabelProp="label"
-      >
-        <Option value="javascript" label="javascript">
-          <div className="demo-option-label-item">JavaScript</div>
-        </Option>
-        <Option value="python" label="python">
-          <div className="demo-option-label-item">Python</div>
-        </Option>
-        <Option value="php" label="php">
-          <div className="demo-option-label-item">PHP</div>
-        </Option>
-      </Select>
+  const handleDeleteSkill = (skillId) => {
+    const newSkills = skills.filter((skill) => skill.id !== skillId);
+    setSkills(newSkills);
+  };
 
-      <button
-        style={{ marginTop: "1rem" }}
-        type="submit"
-        className="btn btn-primary"
-      >
-        Submit
-      </button>
-    </form>
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <input
+            type="text"
+            className="form-control"
+            id="fullName"
+            name="fullName"
+            placeholder="Full Name:"
+            value={data.fullName}
+            onChange={(e) => handleChange(e)}
+          />
+          <span style={{ color: "red", fontSize: "0.7rem" }}>
+            {errors?.errors?.fullName}
+          </span>
+        </div>
+        <br />
+        <div className="form-group">
+          <input
+            type="date"
+            className="form-control"
+            id="birthDate"
+            name="birthDate"
+            placeholder="BirthDate:"
+            value={data.birthDate}
+            onChange={(e) => handleChange(e)}
+          />
+          <span style={{ color: "red", fontSize: "0.7rem" }}>
+            {errors?.errors?.birthdate}
+          </span>
+        </div>
+        <br />
+        <Select
+          showSearch
+          style={{ width: "100%" }}
+          placeholder="Select the skills"
+          optionFilterProp="children"
+          onChange={handleSelectChange}
+          filterOption={(input, option) =>
+            option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+          }
+        >
+          <Option value="javascript">javascript</Option>
+          <Option value="python">python</Option>
+          <Option value="php">php</Option>
+        </Select>
+
+        <div className="skills">
+          {skills.map((skill) => (
+            <div key={skill.id} className="skill-item">
+              {" "}
+              {skill.value}
+              <i
+                className="far fa-trash-alt delete-icon"
+                onClick={() => handleDeleteSkill(skill.id)}
+              ></i>
+            </div>
+          ))}
+        </div>
+
+        <button
+          style={{ marginTop: "1rem" }}
+          type="submit"
+          className="btn btn-primary"
+        >
+          Submit
+        </button>
+      </form>
+    </>
   );
 };
 
