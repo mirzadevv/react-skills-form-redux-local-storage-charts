548740e1c919e8ad016df4e7581e41ed0f5ae1b3
mirzaei.dev <mirzaei.devv@gmail.com>
2021-09-13 21:49:34 +0430

    refactor: using another way for paginate the pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React, { useState } from "react";
 import "./App.css";
 import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
-import { Pagination } from "antd";
 import { useDispatch, useSelector } from "react-redux";
-
 import Form from "./components/Form";
+import Pagination from "./components/Pagination";
+import { paginate } from "./utils/paginate";
 
 const App = () => {
-  const [minValue, setMinValue] = useState(0);
-  const [maxValue, setMaxValue] = useState(3);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize, setPageSize] = useState(3);
   const dispatch = useDispatch();
   const formData = useSelector((state) => state.formData);
+  const paginateFormData = paginate(formData, currentPage, pageSize);
 
-  const handlePaginationChange = (value) => {
-    if (value <= 1) {
-      setMinValue(0);
-      setMaxValue(3);
-    } else {
-      setMinValue(maxValue);
-      setMaxValue(value * 3);
-    }
+  const hanlePageChange = (page) => {
+    console.log(page);
+    setCurrentPage(page);
   };
 
   return (
@@ -31,48 +27,46 @@ const App = () => {
           <p style={{ fontWeight: "bold" }}> THERE ARE NO DATA TO SHOW </p>
         )}
 
-        {formData &&
-          formData.length > 0 &&
-          formData.slice(minValue, maxValue).map((item) => (
-            <div key={item.id}>
-              <div className="item">
-                <div className="item-info">
-                  {" "}
-                  <span className="title">FullName:</span>{" "}
-                  <span className="value">{item.fullName}</span>
-                </div>
-                <div className="item-info">
-                  {" "}
-                  <span className="title">BirthDate:</span>{" "}
-                  <span className="value">{item.birthDate}</span>
-                </div>
-                <div className="item-info">
-                  {" "}
-                  <span className="title">Skills:</span>{" "}
-                  <span className="value">
-                    {item?.skills?.value?.map((item) => (
-                      <span key={item}> {item}, </span>
-                    ))}
-                  </span>
-                </div>
-                <button
-                  onClick={() => dispatch({ type: "delete", payload: item })}
-                  type="button"
-                  className="btn btn-danger"
-                >
-                  Delete
-                </button>
+        {paginateFormData.map((item) => (
+          <div key={item.id}>
+            <div className="item">
+              <div className="item-info">
+                {" "}
+                <span className="title">FullName:</span>{" "}
+                <span className="value">{item.fullName}</span>
               </div>
-              <hr />
+              <div className="item-info">
+                {" "}
+                <span className="title">BirthDate:</span>{" "}
+                <span className="value">{item.birthDate}</span>
+              </div>
+              <div className="item-info">
+                {" "}
+                <span className="title">Skills:</span>{" "}
+                <span className="value">
+                  {item?.skills?.value?.map((item) => (
+                    <span key={item}> {item}, </span>
+                  ))}
+                </span>
+              </div>
+              <button
+                onClick={() => dispatch({ type: "delete", payload: item })}
+                type="button"
+                className="btn btn-danger"
+              >
+                Delete
+              </button>
             </div>
-          ))}
+            <hr />
+          </div>
+        ))}
+        <Pagination
+          itemsCount={formData.length}
+          pageSize={pageSize}
+          currentPage={currentPage}
+          onPageChange={hanlePageChange}
+        />
       </div>
-      <Pagination
-        defaultCurrent={1}
-        defaultPageSize={3}
-        onChange={handlePaginationChange}
-        total={formData.length}
-      />
     </div>
   );
 };
