f47bad7425fa7b28c50dc2139faf284a86a6b6cc
mirzaei.dev <mirzaei.devv@gmail.com>
2021-09-14 17:46:25 +0430

    style: remove some unusable code

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { v4 as uuidv4 } from "uuid";
 import { Select } from "antd";
 const { Option } = Select;
