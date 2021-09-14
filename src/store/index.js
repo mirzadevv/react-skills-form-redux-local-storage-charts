2bf7fc0020026ce0749a5a6ae1c6c113fc4c6a01
mirzaei.dev <mirzaei.devv@gmail.com>
2021-09-14 17:54:59 +0430

    feat: using saveState and loadState functions into the store
    Whenever the state is changed, subscribe will be call, so redux state will be stored into the local storage and by aadding the persistedState into the store, so we can access the permanently  data

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,23 @@
 import { combineReducers, createStore } from "redux";
 import formDataReducer from "./formData/formDataReducer";
+import { saveState, loadState } from "../utils/localStorage";
+
+const persistedState = loadState();
+
 const reducers = combineReducers({
   formData: formDataReducer,
 });
 
 const store = createStore(
   reducers,
+  persistedState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+store.subscribe(() => {
+  saveState({
+    formData: store.getState().formData,
+  });
+});
+
 export default store;
